5949656acc001ae33ad87c64bb997fc0c4586160
mukulchauhan10 <57175128+mukulchauhan10@users.noreply.github.com>
2020-02-18 11:18:01 +0530

    some new

--- Mukul1.java
+++ Mukul1.java
@@ -1,8 +1,11 @@
 lass Mukul1{
 	public static void main(String[]ar){
-		System.out.println("hello1")
-		System.out.println("hello2")
-		System.out.println("hello2")
+		System.out.println("hello1") ;
 		
+	
 	}	
+	
+	void mukul(){
+		
+	}
 }
--- Test1.java
+++ Test1.java
@@ -1,7 +1,8 @@
 class Test1{
 	public static void main(String[]ar){
-		System.out.println("hello1")
-		System.out.println("hello2")
-		System.out.println("hello2")
+		System.out.println("hello1");
+		System.out.println("hello2");
+		System.out.println("hello2");
+		System.out.println("hello4");
 	}	
 }
